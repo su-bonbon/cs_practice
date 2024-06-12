435a3b436d7ed6d600fe0b9fdd62311077bf86ed
Sujin <sujinbonnaud@gmail.com>
2024-06-11 22:53:13 -0500

    import axios lib

--- react-crud/src/components/CreateUser.js
+++ react-crud/src/components/CreateUser.js
@@ -1,9 +1,63 @@
-export default function CreateUser() {
+import { useState } from "react";
+import axios from "axios";
+
+export default function ListUser() {
+
+    const [inputs, setInputs] = useState({})
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:3000/api/user/save', inputs);
+        console.log(inputs);
+    }
     return (
         <div>
-            <h1>List Users</h1>
+            <h1>List User</h1>
+            <form onSubmit={handleSubmit}>
+                <table cellSpacing="10">
+                    <tbody>
+                        <tr>
+                            <th>
+                                <label>Name: </label>
+                            </th>
+                            <td>
+                                <input type="text" name="name" onChange={handleChange}/>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th>
+                                <label>Email: </label>
+                            </th>
+                            <td>
+                                <input type="text" name="email" onChange={handleChange}/>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th>
+                                <label>Mobile: </label>
+                            </th>
+                            <td>
+                                <input type="text" name="mobile" onChange={handleChange}/>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan="2" align ="right">
+                                <button>Save</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
         </div>
-        
+
     )
 }
 
